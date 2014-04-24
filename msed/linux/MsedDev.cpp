84d7402b30a908e1fc79fb16038f46f93325b813
r0m30 <r0m30@r0m30.com>
2014-04-24 01:09:39 -0700

    Let the user know that they need to run as root.

--- msed/linux/MsedDev.cpp
+++ msed/linux/MsedDev.cpp
@@ -43,7 +43,11 @@ MsedDev::MsedDev(const char * devref)
     if ((fd = open(dev, O_RDWR)) < 0) {
         isOpen = FALSE;
         // This is a D1 because diskscan looks for open fail to end scan
-        LOG(D1) << "Error opening device " << dev;
+        LOG(D1) << "Error opening device " << dev << " " << (int32_t) fd;
+        if (-EPERM == fd) {
+            LOG(E) << "You do not have permission to access the raw disk in write mode";
+            LOG(E) << "Perhaps you might try sudo to run as root";
+        }
     }
     else {
         isOpen = TRUE;
