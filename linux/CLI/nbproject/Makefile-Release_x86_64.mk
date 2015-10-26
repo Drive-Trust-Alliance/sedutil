a5f4ddb4e508602d8b723b4fc1b3a020c5f08564
r0m30 <r0m30@r0m30.com>
2015-10-26 12:55:41 -0700

    Update Netbeans project for new classes

--- linux/CLI/nbproject/Makefile-Release_x86_64.mk
+++ linux/CLI/nbproject/Makefile-Release_x86_64.mk
@@ -53,9 +53,9 @@ OBJECTFILES= \
 	${OBJECTDIR}/_ext/843399383/hmac-sha1.o \
 	${OBJECTDIR}/_ext/843399383/memxor.o \
 	${OBJECTDIR}/_ext/843399383/sha1.o \
-	${OBJECTDIR}/_ext/1472/MsedDevOS.o \
+	${OBJECTDIR}/_ext/1472/MsedDevLinuxNvme.o \
 	${OBJECTDIR}/_ext/1472/MsedDevLinuxSata.o \
-	${OBJECTDIR}/_ext/1472/MsedDevLinuxNvme.o
+	${OBJECTDIR}/_ext/1472/MsedDevOS.o
 
 
 # C Compiler Flags
@@ -172,20 +172,20 @@ ${OBJECTDIR}/_ext/843399383/sha1.o: ../../Common/pbdkf2/sha1.c
 	${RM} "$@.d"
 	$(COMPILE.c) -O2 -Werror -I.. -I../../Common -I../../Common/pbdkf2 -std=c11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/843399383/sha1.o ../../Common/pbdkf2/sha1.c
 
-${OBJECTDIR}/_ext/1472/MsedDevOS.o: ../MsedDevOS.cpp 
+${OBJECTDIR}/_ext/1472/MsedDevLinuxNvme.o: ../MsedDevLinuxNvme.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1472
 	${RM} "$@.d"
-	$(COMPILE.cc) -Werror -I.. -I../../Common -I../../Common/pbdkf2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1472/MsedDevOS.o ../MsedDevOS.cpp
+	$(COMPILE.cc) -Werror -I.. -I../../Common -I../../Common/pbdkf2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1472/MsedDevLinuxNvme.o ../MsedDevLinuxNvme.cpp
 
-${OBJECTDIR}/_ext/1472/MsedDevLinuxSata.o: ../MsedDevLinuxSata.cpp
+${OBJECTDIR}/_ext/1472/MsedDevLinuxSata.o: ../MsedDevLinuxSata.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1472
 	${RM} "$@.d"
 	$(COMPILE.cc) -Werror -I.. -I../../Common -I../../Common/pbdkf2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1472/MsedDevLinuxSata.o ../MsedDevLinuxSata.cpp
 
-${OBJECTDIR}/_ext/1472/MsedDevLinuxNvme.o: ../MsedDevLinuxNvme.cpp
+${OBJECTDIR}/_ext/1472/MsedDevOS.o: ../MsedDevOS.cpp 
 	${MKDIR} -p ${OBJECTDIR}/_ext/1472
 	${RM} "$@.d"
-	$(COMPILE.cc) -Werror -I.. -I../../Common -I../../Common/pbdkf2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1472/MsedDevLinuxNvme.o ../MsedDevLinuxNvme.cpp
+	$(COMPILE.cc) -Werror -I.. -I../../Common -I../../Common/pbdkf2 -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1472/MsedDevOS.o ../MsedDevOS.cpp
 
 # Subprojects
 .build-subprojects:
