f2e1ed8f9d5e6754ccfc326a7d1d7639e120a121
r0m30 <r0m30@r0m30.com>
2017-07-11 18:23:18 -0700

    Update images to buildroot 2017.05.1 - Change PBA?? to ??bit for easier tab completion - Remove TinyCore - Rescue now same Kernel/libs as PBA - Add Rescue64 image - UEFI 64 bit image - No debug PBAs as debugging is now built into PBA - create/use buildroot package for sedutil-cli and linuxpba to remove lib version issues - increase size of PBA images to handle larger kernel - increase size of Rescue to include PBA images

--- images/buildroot/PBA64/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm PBA64/target/etc/init.d/*
-sed -i '1,1s/\*//' PBA64/target/etc/shadow
-exit 0
---
+++ images/buildroot/packages/sedutil/sedutil.mk
@@ -0,0 +1,25 @@
+################################################################################
+#
+# sedutil
+#
+################################################################################
+SEDUTIL_VERSION = 1.15
+SEDUTIL_SOURCE = sedutil-$(SEDUTIL_VERSION).tar.gz
+SEDUTIL_SITE = http://www.foosoftware.org/download
+SEDUTIL_INSTALL_STAGING = NO
+SEDUTIL_LIBTOOL_PATCH = NO
+SEDUTIL_INSTALL_TARGET = YES
+SEDUTIL_CONF_OPTS = --sbindir=/sbin
+SWDUTIL_MAKE=$(MAKE1)
+#SEDUTIL_DEPENDENCIES = libstdc++
+# Dont regen version header use the tarball version
+define SEDUTIL_POST_EXTRACT_ACTIONS
+sed -i '/^CLEANFILES/d' $(BUILD_DIR)/sedutil-$(SEDUTIL_VERSION)/Makefile.am
+sed -i '/^BUILT_SOURCES/d' $(BUILD_DIR)/sedutil-$(SEDUTIL_VERSION)/Makefile.am
+sed -i '/^linux\/Version/,3 d' $(BUILD_DIR)/sedutil-$(SEDUTIL_VERSION)/Makefile.am
+sed -i '/^BUILT_SOURCES/d' $(BUILD_DIR)/sedutil-$(SEDUTIL_VERSION)/Makefile.in
+sed -i '/^CLEANFILES/d' $(BUILD_DIR)/sedutil-$(SEDUTIL_VERSION)/Makefile.in
+sed -i '/^linux\/Version/,3 d' $(BUILD_DIR)/sedutil-$(SEDUTIL_VERSION)/Makefile.in
+endef
+SEDUTIL_POST_EXTRACT_HOOKS += SEDUTIL_POST_EXTRACT_ACTIONS
+$(eval $(autotools-package))
