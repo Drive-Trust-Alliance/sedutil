79558fb060dab07d256320a3f2cc6188f97550cd
Andy Sparrow <asparrow@kentik.com>
2018-03-29 16:11:08 -0700

    Remove reboot call, as this is not useful when run from a booted squash

--- LinuxPBA/LinuxPBA.cpp
+++ LinuxPBA/LinuxPBA.cpp
@@ -39,11 +39,11 @@ int main(int argc, char** argv) {
     printf("DTA LINUX Pre Boot Authorization \n");
     string p = GetPassPhrase("Please enter pass-phrase to unlock OPAL drives: ");
     UnlockSEDs((char *)p.c_str());
-    if (strcmp(p.c_str(), "debug")) {
-        printf("Starting OS \n");
-        sync();
-        reboot(RB_AUTOBOOT);
-    }
+    //if (strcmp(p.c_str(), "debug")) {
+        //printf("Starting OS \n");
+        //sync();
+        //reboot(RB_AUTOBOOT);
+    //}
     return 0;
 }
 
