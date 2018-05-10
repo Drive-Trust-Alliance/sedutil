4393dc57763bc587121f2018266a685d30db0be0
jerry hwang <jerry@fidelityheight.com>
2018-05-10 11:01:52 -0700

    remove comment

--- rc/UEFI.bat
+++ rc/UEFI.bat
@@ -1,4 +1,2 @@
 copy ..\windows\CLI\x64\Debug\sedutil-cli.exe sedutil.exe
 copy /b  sedutil.exe + UEFI64.zip sedutil-cli.exe
-REM C:\util-1207\util\rc\sedutil-cli.exe --loadpbaimage password UEFI \\.\physicaldrive1
-REM C:\util-1207\util\rc\sedutil-cli.exe --loadpbaimage 7WTWDD6Q2VF13A35711PQ3MA9ZJXXVT4 UEFI \\.\physicaldrive1
