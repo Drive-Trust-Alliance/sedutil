08876a5036c2caa181b6568a52bf93bcb1420b59
radmanz <cam@helimediagroup.com>
2018-05-31 15:34:46 +0800

    updated unlock PBA to support mac sniffing

---
+++ LinuxPBA/Network.h
@@ -0,0 +1,25 @@
+/* C:B**************************************************************************
+This software is Copyright 2014-2016 IMON Network LTD HK <cam@imonnetwork.com>
+
+This file is part of sedutil and not free, authorisation must be granded.
+
+sedutil is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+sedutil is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with sedutil.  If not, see <http://www.gnu.org/licenses/>.
+
+* C:E********************************************************************** */
+#pragma once
+#include <string>
+using namespace std;
+
+string GetMACAddress(void);
+string GetFirstInterface(void);
