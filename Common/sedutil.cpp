1824266724f2652c0c373ce2575a03ecf35a6594
jerry hwang <jerry@fidelityheight.com>
2018-05-31 15:16:38 -0700

    version 0.3.2

--- Common/sedutil.cpp
+++ Common/sedutil.cpp
@@ -1047,7 +1047,7 @@ int main(int argc, char * argv[])
 		st1 = "macOS";
         #endif
 		
-        printf("Fidelity Lock Version : 0.3.1.%s.%s 20180525-A001\n", st1.c_str(),GIT_VERSION);
+        printf("Fidelity Lock Version : 0.3.2.%s.%s 20180525-A001\n", st1.c_str(),GIT_VERSION);
 		return 0;
 		break;
 	case sedutiloption::hashvalidation:
