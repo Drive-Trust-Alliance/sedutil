255fe5596579a4c561fef785fa1f6cb0f05d24ac
jerry hwang <jerry@fidelityheight.com>
2018-06-14 12:52:24 -0700

    correct obfuscation for audit user

--- Common/ob.h
+++ Common/ob.h
@@ -25,7 +25,7 @@ public:
 	char s100[16]   =  { 'F' ^ Seed,'i' ^ (Seed + 1),'d' ^ (Seed + 2),'e' ^ (Seed + 3),'l' ^ (Seed + 4),'i' ^ (Seed + 5),'t' ^ (Seed + 6),'y' ^ (Seed + 7),'P' ^ (Seed + 8),'R' ^ (Seed + 9),'O' ^ (Seed + 10),'1' ^ (Seed + 11),'0' ^ (Seed + 12), '0' ^ (Seed + 13), ' ' ^ (Seed + 14), ' ' ^ (Seed + 15), };
 	char sunlmt[17] =  { 'F' ^ Seed,'i' ^ (Seed + 1),'d' ^ (Seed + 2),'e' ^ (Seed + 3),'l' ^ (Seed + 4),'i' ^ (Seed + 5),'t' ^ (Seed + 6),'y' ^ (Seed + 7),'P' ^ (Seed + 8),'R' ^ (Seed + 9),'O' ^ (Seed + 10),'U' ^ (Seed + 11),'n' ^ (Seed + 12), 'l' ^ (Seed + 13), 'i' ^ (Seed + 14), 'm' ^ (Seed + 15), 't'};
 	//	char sunlmt[17] =  { 'F' ^ Seed,'i','d','e','l','i','t','y' ^																		,'P','R','O','U','n' ^ (Seed + 12), 'l' ^ (Seed + 13), 'i' ^ (Seed + 14), 'm' ^ (Seed + 15), 't'};
-	char audit[16] = { 'F' ^ Seed,'0' ^ (Seed + 1),'i' ^ (Seed + 2),'D' ^ (Seed + 3),'2' ^ (Seed + 4),'e' ^ (Seed + 5),'l' ^ (Seed + 6),'i' ^ (Seed + 7),'8' ^ (Seed + 8),'1' ^ (Seed + 9),'T' ^ (Seed + 10),'y' ^ (Seed + 11) };
+	char audit[16] = { 'F' ^ Seed,'0' ^ (Seed + 1),'i' ^ (Seed + 2),'D' ^ (Seed + 3),'2' ^ (Seed + 4),'e' ^ (Seed + 5),'l' ^ (Seed + 6),'i' ^ (Seed + 7),'8' ^ (Seed + 8),'1' ^ (Seed + 9),'T' ^ (Seed + 10),'y' ^ (Seed + 11), 0 ^ (Seed + 12), 0 ^ (Seed + 13), 0 ^ (Seed + 14), 0 ^ (Seed + 15), };
 }; 
 
 
