84da59347276beacc43bdcac1626b0ac0fc0601b
Jerry Hwang <jerry@fidelityheight.com>
2018-06-27 21:22:24 -0700

    change window batch to linux syntax

---
+++ linux/PyExtuD.sh
@@ -0,0 +1,10 @@
+
+echo "#ifdef PYEXTLIC" > ../License/pyextlic.h
+echo "#undef PYEXTLIC" >> ../License/pyextlic.h
+echo "#endif" >> ../License/pyextlic.h
+
+echo "#ifdef PYEXTOB" > ../Common/pyextob.h
+echo "#undef PYEXTOB" >> ../Common/pyextob.h
+echo "#endif" >> ../Common/pyextob.h
+
+
