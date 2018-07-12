9b9ecfef6fbad3e65d491ecdcd7078b3b7e24a0a
jerry hwang <jerry@fidelityheight.com>
2018-07-12 10:57:46 -0700

    fix import error undefined symbol: __gxx_personality_v0 ; change module name to PyExtOb from ob

--- ob/buildob.sh
+++ ob/buildob.sh
@@ -8,6 +8,8 @@ echo "#ifndef PYEXTOB" > pyextob.h
 echo "#define PYEXTOB 1"  >> pyextob.h
 echo "#endif" >>  pyextob.h
 
+echo "void * __gxx_personality_v0;" >> pyextob.h
+
 CC=g++ python setup.py build
 
 # build x86_64 ob.so
--- ob/setup.py
+++ ob/setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('ob',
+module = Extension('PyExtOb',
                     sources = ['ob.cpp'],
-                    language = "g++",
-                    extra_compile_args=["-Wno-narrowing",  "-std=c++0x"])
+                    #language = ["g++"],
+                    #linker = ["g++"],
+                    extra_compile_args=["-Wno-narrowing",  "-std=c++0x"],
+                    extra_link_args=["-fno-exceptions"]
+                    )
 
 setup (name = 'PackageName',
        version = '1.0',
        description = 'This is a demo package',
-       ext_modules = [module1])
+       author='Jerry Hwang',
+       author_email='jerry@fidelityheight.com',
+       url='www.fidelityheight.com',
+       ext_modules = [module])
