ecaeae5f7bfa46e7fcde5b892a57405741ecdb01
Jerry Hwang <jerry@fidelityheight.com>
2018-10-24 15:52:44 -0700

    copy so file to py directory

--- ob/buildob.sh
+++ ob/buildob.sh
@@ -1,8 +1,8 @@
 # build ob.so for python GUI
 
-cp ../Common/ob.cpp .
-cp ../Common/ob.h .
-cp ../License/include.h .
+cp -pv ../Common/ob.cpp .
+cp -pv ../Common/ob.h .
+cp -pv ../License/include.h .
 
 echo "#ifndef PYEXTOB" > pyextob.h
 echo "#define PYEXTOB 1"  >> pyextob.h
@@ -11,6 +11,8 @@ echo "#endif" >>  pyextob.h
 echo "void * __gxx_personality_v0;" >> pyextob.h
 
 CC=g++ python setup.py build
+
+cp -pv ./build/lib.linux-x86_64-2.7/PyExtOb.so ../py/
 
 # build x86_64 ob.so
 
