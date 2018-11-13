8b421eb17b705496550d7bbe07af060501bea08c
jerry hwang <jerry@fidelityheight.com>
2018-11-12 16:09:17 -0800

    MX500 need IO_BUFFERSIZE <= 16K

--- Common/DtaConstants.h
+++ Common/DtaConstants.h
@@ -18,9 +18,9 @@ along with sedutil.  If not, see <http://www.gnu.org/licenses/>.
 
  * C:E********************************************************************** */
 /** Length of the IO buffers used */
-#define IO_BUFFER_LENGTH 17408
+#define IO_BUFFER_LENGTH  16384 // 17408(17X1024)->NG  2048 4096 16384(2048X8) OK 18432(2048x9) -> NG need to be less than 16K
 #define IO_BUFFER_LENGTH_HI 61440
-#define IO_BUFFER_LENGTH_LO 17408 
+#define IO_BUFFER_LENGTH_LO 16384 // 17408 --> 2048 4096 16384  OK
 /** Alignment of the IO buffers.
 * generic align on 1k boundary probably not needed
 * but when things weren't working this was one of the 
