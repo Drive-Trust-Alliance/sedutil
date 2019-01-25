671bf1757cb8b32d98ddc8f30c5a025fce71cd98
jerry hwang <jerry@fidelityheight.com>
2019-01-25 10:54:29 -0800

    update gui V0.17.5

--- Common/sedutil.cpp
+++ Common/sedutil.cpp
@@ -1201,8 +1201,8 @@ int main(int argc, char * argv[])
         #if defined(APPLE) || defined(_APPLE) || defined(__APPLE__)
 		st1 = "macOS";
         #endif
-		
-	printf("Fidelity Lock Version : 0.7.5.%s.%s 20190122-A001\n", st1.c_str(),GIT_VERSION);
+
+	printf("Fidelity Lock Version : 0.7.6.%s.%s 20190125-A001\n", st1.c_str(),GIT_VERSION);
 		return 0;
 		break;
 	case sedutiloption::hashvalidation:
