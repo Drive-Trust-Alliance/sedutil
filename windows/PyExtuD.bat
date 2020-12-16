93e22dea4e5269c2908f089c512ba8848b25a373
jerry <jerry@fidelityheight.com>
2020-12-15 16:46:16 -0800

    fix PyExtuD.bat for batch build

--- windows/PyExtuD.bat
+++ windows/PyExtuD.bat
@@ -1,3 +1,7 @@
+@echo off
+@REM this batch file must execute from one level lower which is CLI directory
+
+cd ..
 echo #ifdef PYEXTLIC > ..\License\pyextlic.h
 echo #undef PYEXTLIC >> ..\License\pyextlic.h
 echo #endif >> ..\License\pyextlic.h
@@ -10,3 +14,4 @@ echo #endif >> ..\Common\pyextob.h
 echo #ifdef PYEXTHASH > ..\Common\pyexthash.h
 echo #undef PYEXTHASH >> ..\Common\pyexthash.h
 echo #endif >>  ..\Common\pyexthash.h
+cd CLI
