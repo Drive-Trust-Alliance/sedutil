462a4609ffc3f1341bff8d8a1fb38e2b5ea876a9
jerry <jerry@fidelityheight.com>
2020-12-15 22:00:43 -0800

    take arg for fs.exe add script fs.cmd to get file size from speficied directory

--- rc/fs.cpp
+++ rc/fs.cpp
@@ -1,15 +1,33 @@
 // write file size to sedsize.h
+// how to compile
+// CL /EHsc fs.cpp 
 #include <fstream>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 using namespace std;
-int main () {
+int main (int argc, char * argv[] ) {
+
     char buf[64] = {' '};
-    const char * fname[] = {"sedutil.exe","..\\Common\\sedsize.h"};
+	char exename[64] = { ' ' };
+    const char * fname[] = {"sedutil.exe","..\\Common\\sedsize.h",""};
     ifstream pbafile;
 
-    pbafile.open(fname[0], ios::in | ios::binary); // get sedutil-cli.exe size
+	switch(argc) {
+		case 1 : 
+			strncpy(exename, fname[0], strlen(argv[1]));
+			break;
+		case 2: 
+			strncpy(exename, argv[1], strlen(argv[1]));
+			break;
+		default:
+			strncpy(exename, fname[0], strlen(argv[1]));
+			break;
+
+	}
+	cout << exename << endl;
+
+    pbafile.open(exename, ios::in | ios::binary); // get sedutil-cli.exe size
     pbafile.seekg(0, pbafile.end); // seek to the end of sedutil.exe
     long int pos_beg = pbafile.tellg(); 
     pbafile.close();
