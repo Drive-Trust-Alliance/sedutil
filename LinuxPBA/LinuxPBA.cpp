a27853c7ba5e0a757a718e57f23ec9a45bcdd29b
shizhao.li <shizhao.li@weride.ai>
2021-10-04 12:52:34 -0700

    Test Version

--- LinuxPBA/LinuxPBA.cpp
+++ LinuxPBA/LinuxPBA.cpp
@@ -39,6 +39,7 @@ int main(int argc, char** argv) {
     LOG(D4) << "Legacy PBA start" << endl;
 //    system ("tput clear");
     printf("DTA LINUX Pre Boot Authorization \n");
+    printf("Test Version\n");
     string p = GetPassPhrase("Please enter pass-phrase to unlock OPAL drives: ");
     UnlockSEDs((char *)p.c_str());
     if (strcmp(p.c_str(), "debug")) {
