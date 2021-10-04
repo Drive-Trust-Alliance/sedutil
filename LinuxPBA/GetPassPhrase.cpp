17ebfa1c3654769b9f45afdca2d74d8ac67404bf
shizhao.li <shizhao.li@weride.ai>
2021-10-04 16:26:08 -0700

    change printf

--- LinuxPBA/GetPassPhrase.cpp
+++ LinuxPBA/GetPassPhrase.cpp
@@ -80,7 +80,7 @@ string GetPassPhrase()
   }
   printf("%s\n", uuid);
   string password(uuid);
-  printf("size: %d\n", password.size());
+  printf("size: %lu\n", password.size());
   while((ch = getchar()) != 10) {}
   return password;
 }
