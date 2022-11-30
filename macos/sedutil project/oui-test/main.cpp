b4137dace55f82b47573739dc6ae19e065b87a8a
Scott Marks <scott@magnolia-heights.com>
2022-11-30 13:44:34 -0500

    canonical

--- macos/sedutil project/oui-test/main.cpp
+++ macos/sedutil project/oui-test/main.cpp
@@ -12,31 +12,30 @@
 
 int main(int argc, const char * argv[]) {
     bool thereHaveBeenErrors = false;
-    for (int i=1; i<argc; i++) {
+    for (int i=1; i<argc; std::cout << std::endl, i++) {
         const char * oui(argv[i]);
+
         CString_Lookup_Table::const_iterator pManufacturer = manufacturer_for_oui.find(oui);
         CString_Lookup_Table::const_iterator moEnd         = manufacturer_for_oui.end();
-        if (pManufacturer != moEnd) {
-            std::cout << oui << " => " << pManufacturer->second << std::endl;
-
-
-            const char * vendor(pManufacturer->second);
-            CString_Lookup_Table::const_iterator pVendorID = vendorID_for_vendor.find(vendor);
-            CString_Lookup_Table::const_iterator vIvEnd    = vendorID_for_vendor.end();
-            if (pVendorID != vIvEnd) {
-                std::cout << vendor << " => " << pVendorID->second << std::endl;
-            } else {
-                std::cerr << vendor << " *** not found ***"  << std::endl;
-                thereHaveBeenErrors = true;
-            }
-
-
-        } else {
+        if (pManufacturer == moEnd) {
             std::cerr << oui << " *** not found ***"  << std::endl;
             thereHaveBeenErrors = true;
+            continue;
         }
         
+        const char * vendor(pManufacturer->second);
+        std::cout << oui << " => " << vendor << std::endl;
 
+        CString_Lookup_Table::const_iterator pVendorID = vendorID_for_vendor.find(vendor);
+        CString_Lookup_Table::const_iterator vIvEnd    = vendorID_for_vendor.end();
+        if (pVendorID == vIvEnd) {
+            std::cerr << vendor << " *** not found ***"  << std::endl;
+            thereHaveBeenErrors = true;
+            continue;
+        }
+        
+        const char * vendorID = pVendorID->second ;
+        std::cout << vendor << " => " << vendorID << std::endl;
     }
     return thereHaveBeenErrors ? 1 : 0;
 }
