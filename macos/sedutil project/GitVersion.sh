c1f90e26dc64e145ac6bd6e4658fb5b29343ee0c
Scott Marks <scott@magnolia-heights.com>
2023-03-02 20:28:07 -0500

    fixing bad variable name

--- macos/sedutil project/GitVersion.sh
+++ macos/sedutil project/GitVersion.sh
@@ -8,8 +8,8 @@ else
     GITVER=tarball
 fi
 
-which="$( echo "${BUILT_PRODUCTS_DIR}" | cut -c "$(expr ${#BUILD_ROOT} + 2)-${#BUILT_PRODUCTS_DIR}" | awk '{print $1}' )"
-if [ "${which}" == "Debug" ]
+configuration="$( echo "${BUILT_PRODUCTS_DIR}" | cut -c "$(expr ${#BUILD_ROOT} + 2)-${#BUILT_PRODUCTS_DIR}" | awk '{print $1}' )"
+if [ "${configuration}" == "Debug" ]
 then
     GITVER="${GITVER} (Debug build)"
 fi
