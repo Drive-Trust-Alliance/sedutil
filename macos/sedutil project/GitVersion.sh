50b8458f35a473cc977db8c43c2acf34c6f553b2
Scott Marks <scott@magnolia-heights.com>
2023-03-02 20:21:50 -0500

    GitVersion

--- macos/sedutil project/GitVersion.sh
+++ macos/sedutil project/GitVersion.sh
@@ -1,9 +1,17 @@
 #!/bin/bash 
 # set -x
-GIT=`which git`
-if [ "x"${GIT} == "x" ]; then
-	echo "#define GIT_VERSION \"tarball\""
+
+if which -s git
+then
+    GITVER=`git describe --dirty`
 else
-	GITVER=`git describe --dirty`
-	echo "#define GIT_VERSION " \"$GITVER\"
+    GITVER=tarball
 fi
+
+which="$( echo "${BUILT_PRODUCTS_DIR}" | cut -c "$(expr ${#BUILD_ROOT} + 2)-${#BUILT_PRODUCTS_DIR}" | awk '{print $1}' )"
+if [ "${which}" == "Debug" ]
+then
+    GITVER="${GITVER} (Debug build)"
+fi
+
+echo "#define GIT_VERSION \"${GITVER}\""
