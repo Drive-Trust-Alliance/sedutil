25eaaede8ecee8cbc4dd09489830f4ebf4490e28
Scott Marks <scott@magnolia-heights.com>
2023-12-08 18:24:04 -0700

    gConfiguration is UInt8

--- macos/sedutil project/reenumerate/reenumerate.c
+++ macos/sedutil project/reenumerate/reenumerate.c
@@ -121,7 +121,7 @@ ParseArguments ( int argc, const char * argv[] )
                 
             case 'c':
                 gDoSetConfiguration = TRUE;
-                gConfiguration = (uint32_t)strtoul(optarg, NULL, 0);    // is this safe?
+                gConfiguration = (UInt8)strtoul(optarg, NULL, 0);    // is this safe?
                 break;
                 
                 
@@ -148,6 +148,7 @@ ParseArguments ( int argc, const char * argv[] )
     }
 }
 
+static
 void ProcessDevice(io_service_t aDevice)
 {
     IOCFPlugInInterface     **plugInInterface;
