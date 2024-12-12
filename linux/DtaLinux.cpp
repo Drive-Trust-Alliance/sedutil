5bbe4ff75b9416926d157a755d9760f7ff4e3904
Scott Marks <scott@magnolia-heights.com>
2024-12-11 22:59:51 -0500

    linux/DtaLinux.cpp

--- linux/DtaLinux.cpp
+++ linux/DtaLinux.cpp
@@ -218,15 +218,19 @@ DtaOS::dictionary * DtaLinux::getOSSpecificInformation(OSDEVICEHANDLE osDeviceHa
         str_WWN=str_WWN.substr(4);
     }
     size_t WWN_length=str_WWN.length();
+    LOG(D3) << "str_WWN is " << str_WWN;
+    LOG(D3) << "str_WWN.length()=" << str_WWN.length();
+
     size_t device_info_nybbles = 2 * sizeof(device_info.worldWideName);
+    LOG(D3) << "device_info_nybbles=" << device_info_nybbles;
 
     intptr_t length_difference =  device_info_nybbles - 1 - WWN_length ;
-
+    LOG(D3) << "length_difference=" << length_difference;
 
     if (0 < length_difference) {
       str_WWN += std::string(length_difference, '0');
     } else {
-      str_WWN = str_WWN.substr(length_difference, device_info_nybbles - 1);
+      str_WWN = str_WWN.substr(-length_difference, device_info_nybbles - 1);
     }
     str_WWN = std::string(1, '5') + str_WWN;
 
